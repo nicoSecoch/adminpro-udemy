33364536102ba8c7017e980554407a0ac2b9538f
Nicolas Torres <nicolas.tvasquez@gmail.com>
2019-08-20 17:04:34 -0400

    mod en rutas y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ProgressComponent } from './components/progress/progress.component';
-import { Graficas1Component } from './components/graficas1/graficas1.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { APP_RPUTES } from './app.routes';
-import { PagesComponent } from './components/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './components/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    HeaderComponent,
-    PagesComponent,
+    LoginComponent,    
     RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    APP_RPUTES
+    APP_RPUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
